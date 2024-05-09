e99cabe0a475bec0e2db2fe079f38f0dafa545d2
João Augusto Rosa Feltran <j174083@dac.unicamp.br>
2024-05-09 00:31:29 -0300

    added send_to_client

--- Lab02/src/server/server.c
+++ Lab02/src/server/server.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "../music/music.h"
 #include "../external_files/storage_handler.h"
@@ -274,6 +279,60 @@ again:
         perror("str_echo: read error");
 }
 
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int send_to_client(configuration serverConfig, char *message) {
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    char strPort[100] = "";
+
+    strcpy(strPort, serverConfig.port);
+    strPort[strcspn(strPort, "\n")] = 0;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(serverConfig.ip, strPort, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+        
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return 2;
+    }
+
+    if ((numbytes = sendto(sockfd, message, strlen(message), 0, p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, serverConfig.ip);
+    close(sockfd);
+    return 0;
+}
+
 
 int main() {
     configuration serverConfig;
@@ -313,6 +372,8 @@ int main() {
             exit(0);
         }
         printf("Connection established.\n");
+
+        send_to_client(serverConfig, "Teste abc batata\n");
 
         // Criação de um processo filho para tratar a conexão
         if ((childpid = fork()) == 0) { /* processo filho */
